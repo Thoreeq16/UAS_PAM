57f5d9bb44f1e01ea8321ca09da04447e180e4b1
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2024-01-06 01:39:11 +0700

    mengisi parameter untuk interface RepositoriCatatanPemantauan

--- app/src/main/java/com/example/uas_paw/repositori/RepositoriCatatanPemantauan.kt
+++ app/src/main/java/com/example/uas_paw/repositori/RepositoriCatatanPemantauan.kt
@@ -1,4 +1,17 @@
 package com.example.uas_paw.repositori
 
+import com.example.uas_paw.data.CatatanPemantauan
+import com.example.uas_paw.data.SensorTanaman
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoriCatatanPemantauan {
+    fun getAllCatatanPemantauanStream(): Flow<List<CatatanPemantauan>>
+
+    fun getCatatanPemantauanStream(id: Int): Flow<CatatanPemantauan?>
+
+    suspend fun insertCatatanPemantauan(catatanPemantauan: CatatanPemantauan)
+
+    suspend fun deleteCatatanPemantauan(catatanPemantauan: CatatanPemantauan)
+
+    suspend fun updateCatatanPemantauan(catatanPemantauan: CatatanPemantauan)
 }
