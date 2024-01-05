983b5446562ca143fb9458355629576b31262316
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2024-01-06 01:29:22 +0700

    membuat parameter pada interface RepositoriTanaman

--- app/src/main/java/com/example/uas_paw/repositori/RepositoriTanaman.kt
+++ app/src/main/java/com/example/uas_paw/repositori/RepositoriTanaman.kt
@@ -1,4 +1,16 @@
 package com.example.uas_paw.repositori
 
+import com.example.uas_paw.data.Tanaman
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoriTanaman {
+    fun getAllTanamanStream(): Flow<List<Tanaman>>
+
+    fun getTanamanStream(id: Int): Flow<Tanaman?>
+
+    suspend fun insertTanaman(tanaman: Tanaman)
+
+    suspend fun deleteTanaman(tanaman: Tanaman)
+
+    suspend fun updateTanaman(tanaman: Tanaman)
 }
