b960ef0f813483ec0b69d260b8fbb185605f1962
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2024-01-06 01:57:56 +0700

    membuat data class HomeUiState dan class HomeViewModel dan membuat parameter dan method

--- app/src/main/java/com/example/uas_paw/model/HomeViewModel.kt
+++ app/src/main/java/com/example/uas_paw/model/HomeViewModel.kt
@@ -1,3 +1,30 @@
 package com.example.uas_paw.model
 
-class HomeViewModel() {}
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uas_paw.data.Tanaman
+import com.example.uas_paw.repositori.RepositoriTanaman
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriTanaman: RepositoriTanaman) : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriTanaman.getAllTanamanStream().filterNotNull()
+        .map { HomeUiState(listTanaman = it.toList()) }.stateIn(
+            scope = viewModelScope, started = SharingStarted.WhileSubscribed(
+                TIMEOUT_MILLIS
+            ), initialValue = HomeUiState()
+        )
+
+
+}
+
+data class HomeUiState(
+    val listTanaman: List<Tanaman> = listOf()
+)
